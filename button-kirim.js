88e9a937da16e0331127cc52520370f0108a73ab
jesper245 <90240039+jesper245@users.noreply.github.com>
2022-01-18 12:37:34 +0700

    new deploy

---
+++ button-kirim.js
@@ -0,0 +1,4 @@
+function kirimpesan() {
+    alert("Your massage has sent to us");
+    alert("Thank you for your feedback")
+}
---
+++ checkout.js
@@ -0,0 +1,3 @@
+function kirimpesan() {
+    alert("Your Order has been proses");
+    alert("Thank you for your order")}
